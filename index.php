dc163cd7d9a08f701c949a1d7065b8f31c9d15d6
munir-abbasi <22575093+munir-abbasi@users.noreply.github.com>
2025-06-06 14:18:26 +0500

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
  * Copyright (c) 2021 Madi Nuralin
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
+ * Modified by: Munir Abbasi
+ * Theme URL: https://github.com/munir-abbasi/material-theme-modifed
+ * Author Website: https://syntaxhouse.com
+ *
  * @ingroup plugins_themes_material
  *
  * @brief Wrapper for material theme plugin.
